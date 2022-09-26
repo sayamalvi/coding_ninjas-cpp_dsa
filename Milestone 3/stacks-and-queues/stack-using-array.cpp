fedea1937297a25b25f14d69853e4e8e65d4fa83
Sayam <sayamalvi07@gmail.com>
2022-09-26 21:54:47 +0530

    stacks using array

---
+++ Milestone 3/stacks-and-queues/stack-using-array.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+class ArrayStack
+{
+    int *data;
+    int nextIndex;
+    int capacity;
+
+public:
+    ArrayStack(int totalSize)
+    {
+        data = new int[totalSize];
+        nextIndex = 0;
+        capacity = totalSize;
+    }
+
+    int size() { return nextIndex; }
+
+    bool isEmpty() { return nextIndex == 0; }
+
+    void push(int element)
+    {
+        if (nextIndex == capacity)
+        {
+            cout << "Stack is full";
+            return;
+        }
+        data[nextIndex] = element;
+        nextIndex++;
+    }
+
+    int pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty";
+            return INT_MIN;
+        }
+        nextIndex--;
+        return data[nextIndex];
+    }
+    int top()
+    {
+        if (isEmpty() == 0)
+            return INT_MIN;
+        return data[nextIndex - 1];
+    }
+};
+int main()
+{
+    ArrayStack s(4);
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    s.push(50);
+    cout << s.top() << endl;
+
+    cout << s.pop() << endl;
+    cout << s.pop() << endl;
+    cout << s.pop() << endl;
+
+    cout << s.size() << endl;
+    cout << s.isEmpty() << endl;
+}
