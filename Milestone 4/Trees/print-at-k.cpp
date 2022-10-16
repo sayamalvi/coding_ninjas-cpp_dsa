a139101f6c42cf73cae93c04a08937503049b2da
Sayam <sayamalvi07@gmail.com>
2022-10-16 23:07:29 +0530

    .

---
+++ Milestone 4/Trees/print-at-k.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "TreeNode.h"
+using namespace std;
+TreeNode<int> *takeInput()
+{
+    int rootData;
+    cout << "Enter data" << endl;
+    cin >> rootData;
+    TreeNode<int> *root = new TreeNode<int>(rootData);
+    int n;
+    cout << "Enter number of children of" << rootData << endl;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        TreeNode<int> *child = takeInput();
+        root->children.push_back(child);
+    }
+    return root;
+}
+void printAtLevelK(TreeNode<int> *root, int k)
+{
+    if (root == NULL)
+        return;
+    if (k == 0)
+    {
+        cout << root->data << " ";
+    }
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        printAtLevelK(root->children[i], k - 1);
+    }
+}
+int main()
+{
+    TreeNode<int> *root = takeInput();
+    int k;
+    cout << "Enter level";
+    cin >> k;
+    printAtLevelK(root, k);
+}
