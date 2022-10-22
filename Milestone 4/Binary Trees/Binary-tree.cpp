52ae66c8e36c1a4d4245fc2c6cc8af0cf1ca322d
Sayam <sayamalvi07@gmail.com>
2022-10-22 21:22:00 +0530

    .

--- Milestone 4/Binary Trees/Binary-tree.cpp
+++ Milestone 4/Binary Trees/Binary-tree.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "BinaryTree.h"
+#include <queue>
 using namespace std;
 BinaryTree<int> *takeInput()
 {
@@ -14,6 +15,42 @@ BinaryTree<int> *takeInput()
     BinaryTree<int> *rightChild = takeInput();
     root->left = leftChild;
     root->right = rightChild;
+    return root;
+}
+BinaryTree<int> *takeInputLevelWise()
+{
+    int rootData;
+    cout << "Enter data" << endl;
+    cin >> rootData;
+    if (rootData == -1)
+        return NULL;
+    BinaryTree<int> *root = new BinaryTree<int>(rootData);
+
+    queue<BinaryTree<int> *> pendingNodes;
+    pendingNodes.push(root);
+    while (!pendingNodes.empty())
+    {
+        BinaryTree<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << "Enter left child of " << front->data << endl;
+        int leftChildren;
+        cin >> leftChildren;
+        if (leftChildren != -1)
+        {
+            BinaryTree<int> *child = new BinaryTree<int>(leftChildren);
+            front->left = child;
+            pendingNodes.push(child);
+        }
+        cout << "Enter right child of " << front->data << endl;
+        int rightChildren;
+        cin >> rightChildren;
+        if (rightChildren != -1)
+        {
+            BinaryTree<int> *child = new BinaryTree<int>(rightChildren);
+            front->right = child;
+            pendingNodes.push(child);
+        }
+    }
     return root;
 }
 void printTree(BinaryTree<int> *root)
